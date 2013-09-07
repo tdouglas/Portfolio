4e80690407a70c17c0335c31f6317726c472d67d
tdouglas <tdouglas@seamlessTL.com>
2013-09-07 19:20:12 -0400

    hard-coding responsive site :/

---
+++ javascripts/main.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+  $('.bxslider').bxSlider();
+
+// function rotateHeading() {
+//   $('#design-font').hide()
+//   $('#develop-font').hide()
+//   setTimeout(function()(hideT, 500);
+//   setTimeout(function()(showDi),500);
+//   setTimeout(function()(hideDi),500);
+//   setTimeout(function()(showDe),500);
+//   setTimeout(function()(hideDe),500);
+
+//   setTimeout(init, 1200); // postponing init until the box falls
+// }
+
+// });
+
+
+});
+
+/*
+$(window).scrollTop()
+1182
+$(window).height()
+382
+$('.intro).offset().top;
+SyntaxError: Unexpected token ILLEGAL
+$('.brief-intro).offset().top;
+SyntaxError: Unexpected token ILLEGAL
+$(window).height()
+382
+data-appear-top-offset="600"
+*/
+
+/*
+$(".brief-intro").on("appear", function(event, $other) {
+  alert("working");
+*/
